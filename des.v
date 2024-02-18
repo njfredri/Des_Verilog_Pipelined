03dedfd86faa6cc1df7ad9d7c22bf5d07cdcd9f6
Nathaniel Fredricks <njfredri@uark.edu>
2024-02-18 13:38:57 -0600

    wrong dir again

--- des.v
+++ des.v
@@ -1023,6 +1023,41 @@ module Flipflop_64(input clk, input rst, input[63:0] in, output reg [63:0] out);
     end
 endmodule
 
+module Flipflop_1(input clk, input rst, input in, output reg out);
+    always@(posedge(clk)) begin
+        if(rst==1) begin
+            out<=0;
+        end
+        else begin
+            out<=in;
+        end
+    end
+endmodule
+
+module Validation_Pipeline (input clk, input rst, input iv, output ov);
+    wire w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13, w14, w15, w16, w17, w18;
+	assign w0 = iv;
+	Flipflop_1 ff_0(clk, rst, w0, w1);
+	Flipflop_1 ff_1(clk, rst, w1, w2);
+	Flipflop_1 ff_2(clk, rst, w2, w3);
+	Flipflop_1 ff_3(clk, rst, w3, w4);
+	Flipflop_1 ff_4(clk, rst, w4, w5);
+	Flipflop_1 ff_5(clk, rst, w5, w6);
+	Flipflop_1 ff_6(clk, rst, w6, w7);
+	Flipflop_1 ff_7(clk, rst, w7, w8);
+	Flipflop_1 ff_8(clk, rst, w8, w9);
+	Flipflop_1 ff_9(clk, rst, w9, w10);
+	Flipflop_1 ff_10(clk, rst, w10, w11);
+	Flipflop_1 ff_11(clk, rst, w11, w12);
+	Flipflop_1 ff_12(clk, rst, w12, w13);
+	Flipflop_1 ff_13(clk, rst, w13, w14);
+	Flipflop_1 ff_14(clk, rst, w14, w15);
+	Flipflop_1 ff_15(clk, rst, w15, w16);
+	Flipflop_1 ff_16(clk, rst, w16, w17);
+	Flipflop_1 ff_17(clk, rst, w17, w18);
+	Flipflop_1 ff_18(clk, rst, w18, ov);
+endmodule
+
 module des (
     input clock,
     input reset, //synchronous high
@@ -1030,8 +1065,8 @@ module des (
     input [63:0] plaintext,
     input iv, //says that there is a valid input
     output [63:0] ciphertext,
-    output reg ov
-);
+    output ov
+	);
     //beginning
     wire[63:0] input_reg_out;
     Flipflop_64 input_reg(
@@ -1040,6 +1075,8 @@ module des (
         .in(plaintext),
         .out(input_reg_out)
     );
+	//Validation_Pipeline (input clk, input rst, input iv, output ov);
+	Validation_Pipeline valid_pipeline(clock, reset, iv, ov);
 
     wire [47:0] keys[16:1]; //keys that will be generated later. keys[layer#][key#] //don't need 0
 
@@ -1179,6 +1216,7 @@ module des (
     //InverseP(input [63:0] in, output [55:0] out);
     wire [63:0] final_text;
     InverseP final_perm({r16_reg_out, l16_reg_out}, final_text);
+	// assign 
     Flipflop_64 output_reg(clock, reset, final_text, ciphertext);
 
 endmodule
--- test_bench.v
+++ test_bench.v
@@ -7,6 +7,7 @@ module testbench();
     reg [63:0] key, plaintext;
     reg iv;
     wire [63:0] ciphertext;
+    reg [63:0] expected_output;
     wire ov;
 
     des des(
@@ -29,15 +30,64 @@ module testbench();
         forever #(clk_tgl_period) clock = ~clock;
     end
 
+    // feed three plaintext blocks to the DES circuit in three consecutive clock cycles. 
+    // Wait for one clock clock, feed one plaintext block.
+    // Wait for one clock clock, feed two plaintext blocks in two consecutive clock cycles. 
+    // Totally sixplaintext blocks are fed to the DES circuit. 
+    // The iv input signal needs to set properly to indicate these six valid input data blocks.
+
     initial begin
         reset = 0;
-        key = 64'b0001001100110100010101110111100110011011101111001101111111110001;
-        plaintext = 64'b0100100101101110001000000110111101101100011001000110010101101110;
+        iv = 0;
+        key = 64'h133457799BBCDFF1;
+        plaintext = 64'h0000000000000000;
         #(full_clk_period) //half a clock cycle
         reset = 1;
         #(full_clk_period)
+        // feed three plaintext blocks to the DES circuit in three consecutive clock cycles. 
         reset = 0;
+        plaintext = 64'h496E206F6C64656E;
+        iv = 1; //ov should be 1 as soon as output is generated
         #(full_clk_period)
+        plaintext = 64'h2074696D65732077;
         iv = 1;
+        #(full_clk_period)
+        plaintext = 64'h68656E2077697368;
+        iv = 1;
+        //Wait for one clock clock, feed one plaintext block.
+        #(full_clk_period) 
+        iv=0;
+        #(full_clk_period)
+        plaintext = 64'h696E67207374696C;
+        iv=1;
+        // Wait for one clock clock, feed two plaintext blocks in two consecutive clock cycles. 
+        #(full_clk_period)
+        iv=0;
+        #(full_clk_period)
+        plaintext = 64'h6C2064696420736F;
+        iv=1;
+        #(full_clk_period)
+        plaintext = 64'h6D6520676F6F6420;
+        iv=1;
+        #(full_clk_period)
+        plaintext = 64'h0000000000000000;
+        iv=0;
+    end
+
+    initial begin
+        expected_output=0;
+        //first output at 205ns
+        #205
+        expected_output=64'hF095CDC9B8A0AD83;
+        #(full_clk_period)
+        expected_output=64'h4109113BAD212ADE;
+        #(full_clk_period)
+        expected_output=64'h7ECF0E52ADD999DE;
+        #(full_clk_period*2)
+        expected_output=64'h7EB3D91ED1D4CFCE;
+        #(full_clk_period*2)
+        expected_output=64'h3DDBACAC1B9F5C23;
+        #(full_clk_period)
+        expected_output=64'h3F40E069D50EAA11;
     end
 endmodule
